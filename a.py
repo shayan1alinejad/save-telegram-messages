e25e9d481948fb4990542df347d5908a36b1ee52
shayan alinejad <76965744+shayan1alinejad@users.noreply.github.com>
2024-01-09 11:42:26 -0800

    Update a.py
    add some extra things

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot("توکن بات دریافت شده از باتفادر")
 ##اکو کردن کل مسیج ها
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-  bot.reply_to(message, "sepi jon save shod")
+  bot.reply_to(message, "saved to database")
   with open('Saved.txt', 'a') as f: 
     f.write(message.from_user.first_name+" Ino Ferestad: "+message.text+"\n") 
 
